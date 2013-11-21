0deb23b0276d4e45e21518211e69585c58926645
Theodore Li <theo@ubuntu.(none)>
2013-11-21 14:40:36 -0800

    New Movies up along with images. 5 Total now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -54,7 +54,7 @@ angular.module('leslieApp')
       'images/days-of-being-wild.jpg',
       1990,
       1,
-      'Teddy boy',
+      'Days of Being Wild (A Fei jingjyuhn) is a 1990 Hong Kong film directed by Wong Kar-wai. The film stars some of the best-known actors and actresses in Hong Kong, including Leslie Cheung, Andy Lau, Maggie Cheung, Carina Lau, Jacky Cheung and Tony Leung Chiu-Wai. Days of Being Wild also marks the first collaboration between Wong and cinematographer Christopher Doyle, with whom he has since made eight films.[2][3] The movie forms the first part of an informal trilogy, together with In the Mood for Love (released in 2000) and 2046 (released in 2004).',
       ['Maggie Cheung', 'Andy Lau', 'Carina Lau', 'Jacky Cheung', 'Tony Leung Chiu-Wai'],
       {
         1991: {
@@ -78,16 +78,68 @@ angular.module('leslieApp')
         }
       }
     );
+    moviesEnglish.addMovie(
+      'Farewell My Concubine',
+      'images/farewell-my-concubine.jpg',
+      1993,
+      3,
+      'Classic, Bawang Bie Ji. Central works of the Fifth generation movement',
+      ['Leslie Cheung', 'Gong Li'],
+      {
+        1992: {
+          'National Board of Review (USA)': ['Best Foreign Film']
+        },
+        1993: {
+          'Cannes Film Festival': ["Palme d'OR", "FIPRESCI Awawrd for Best Film in Competition"],
+          'Mainichi Film Concours': ['Best Foreign Language Film'],
+          'Golden Globe Awards': ['Best Foreign Language Film'],
+          'Chinese Performance Art Association': ['Special Award - Leslie Cheung']
+        },
+        1994: {
+          'BAFTA': ['Best Film not in the English Language'],
+          'Japanese Critics Society': ['Best Actor Award for Foreign Movie - Leslie Cheung']
+        }
+      }
+    );
+    moviesEnglish.addMovie(
+      'Viva Erotica',
+      'images/viva-erotica.jpg',
+      1996,
+      4,
+      'Shu Qi debuts',
+      ['Leslie Cheung', 'Karen Mok', 'Shu Qi'],
+      {
+        1997: {
+          'Hong Kong Film Awards': ['Best Supporting Actress - Shu Qi', 'Best New Artist (Shu Qi)']
+        }
+      }
+    );
+    moviesEnglish.addMovie(
+      'Rouge',
+      'images/rouge.jpg',
+      1988,
+      5,
+      'Chan Chen-Pang, better known as the "12th Young Master" (played by Leslie Cheung), was a fashionable playboy who frequented the opium dens prevalent in Hong Kong in 1934, where he met the high-class and much sought-after courtesan, Fleur (Anita Mui).',
+      ['Leslie Cheung', 'Anita Mui'],
+      {
+        1988: {
+          'Nantes Three Continents Festival': ['Golden Montgolfiere'],
+          'Golden Horse Film Festival': ['Best Actress (Anita Mui)']
+        },
+        1989: {
+          'Asia-Pacific Film Festival': ['Best Actress (Anita Mui)'],
+          'Hong Kong Film Awards': ['Best Actress (Anita Mui)', 'Best Director (Stanley Kwan)', 'Best Film Editing', 'Best Original Film Score', 'Best Original Film Song ("Yin Ji Kau")', 'Best Picture']
+        }
+      }
+    );
 
-    console.log(moviesEnglish);
     $scope.moviesEnglish = moviesEnglish;
     //$scope.currentBelt = 1; // this needs to be considered later with proper DB design
 
-    $scope.clickHandler = function(e) {
+    $scope.beltSelectHandler = function(e) {
       console.log(e);
       // probably need a scope apply here
       $scope.currentBelt = e;
-      console.log($scope.currentBelt);
     };
 
   });
